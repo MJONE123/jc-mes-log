4378809e9e8b76a721e6c70606ca3b3a373c342a
문준일 <answnsdlfwkd@naver.com>
2025-12-12 16:02:38 +0900

    feat: 프로젝트 생성

---
+++ src/test/java/com/jc/log_api/LogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jc.log_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
