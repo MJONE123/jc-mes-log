bc953e20a4967d99be46c6ab6e5339184ce6b24e
문준일 <answnsdlfwkd@naver.com>
2025-12-12 16:55:46 +0900

    fix: 애플리케이션 스케줄 어노테이션 추가

--- src/main/java/com/jc/log_api/LogApiApplication.java
+++ src/main/java/com/jc/log_api/LogApiApplication.java
@@ -2,12 +2,13 @@ package com.jc.log_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling     // @Scheduled 사용을 위해 추가
 public class LogApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LogApiApplication.class, args);
 	}
-
 }
